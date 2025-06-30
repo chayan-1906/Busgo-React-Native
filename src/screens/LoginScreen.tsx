37acb374effaeb9c51c9cbfc8a2347e82411cc84
System-Edge <padmanabhadas9647@gmail.com>
2025-06-30 12:35:34 +0530

    modified package-name: com.busgo -> com.pdas9647.busgo

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -37,12 +37,12 @@ function LoginScreen() {
 
     return (
         <View className={'gap-4'}>
-            <Image source={require('../assets/images/cover.jpeg')} className={'w-full h-64 bg-cover'} />
+            <Image source={require('../assets/images/cover.jpeg')} className={'w-full h-64 bg-cover'}/>
             <View className={'p-4'}>
                 <Text className={'font-okra-medium text-xl text-center'}>Create Account or Sign In</Text>
                 <View className={'flex-row items-center my-4 mt-12 px-2 border border-black gap-2 rounded-md'}>
                     <Text className={'w-[10%] font-okra-bold text-base'}>+91</Text>
-                    <TextInput value={phone} onChangeText={setPhone} maxLength={10} keyboardType={'number-pad'} placeholder={'Enter 10 digit phone number'} className={'font-okra h-11 w-[90%]'} />
+                    <TextInput value={phone} onChangeText={setPhone} maxLength={10} keyboardType={'number-pad'} placeholder={'Enter 10 digit phone number'} className={'font-okra h-11 w-[90%]'}/>
                 </View>
 
                 <TouchableOpacity onPress={handleGoogleSignIn} className={'my-4 py-3 px-4 bg-tertiary rounded-xl justify-center items-center'}>
@@ -53,10 +53,10 @@ function LoginScreen() {
 
                 <View className={'flex-row items-center justify-center gap-4'}>
                     <TouchableOpacity className={'border border-gray-300 rounded-md p-2'} onPress={handleGoogleSignIn}>
-                        <Image source={require('../assets/images/google.png')} resizeMode={'contain'} className={'size-5'} />
+                        <Image source={require('../assets/images/google.png')} resizeMode={'contain'} className={'size-5'}/>
                     </TouchableOpacity>
                     <TouchableOpacity className={'border border-gray-300 rounded-md p-2'}>
-                        <Image source={require('../assets/images/apple.png')} resizeMode={'contain'} className={'size-5'} />
+                        <Image source={require('../assets/images/apple.png')} resizeMode={'contain'} className={'size-5'}/>
                     </TouchableOpacity>
                 </View>
 
