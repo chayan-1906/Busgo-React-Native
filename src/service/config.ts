109de541ad488c51678a04bfc715ce75bdffb5b6
pdas9647 <padmanabhadas9647@gmail.com>
2025-07-09 20:50:02 +0530

    generated apk, v2.0.0

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://busgo-node-js.onrender.com/api/v1';
-export const BASE_URL = 'http://192.168.1.110:4000/api/v1';
+export const BASE_URL = 'https://busgo-node-js.onrender.com/api/v1';
+// export const BASE_URL = 'http://192.168.1.110:4000/api/v1';
 // export const BASE_URL = 'http://localhost:4000/api/v1';
 
 export const WEB_GOOGLE_CLIENT_ID = process.env.EXPO_PUBLIC_WEB_GOOGLE_CLIENT_ID;
